0eecab97a6eeff0eca305e1cd25b94147a389851
juan <jmrivasconcepcion@gmail.com>
2024-09-20 11:52:59 +0200

    simple androidtest

--- app/src/androidTest/java/es/unican/moviesapp/activities/MainActivityTest.java
+++ app/src/androidTest/java/es/unican/moviesapp/activities/MainActivityTest.java
@@ -1,9 +1,9 @@
-package es.unican.moviesapp;
-
+package es.unican.moviesapp.activities;
 
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -16,7 +16,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import es.unican.moviesapp.activities.MainActivity;
+import es.unican.moviesapp.R;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -29,6 +29,11 @@ public class MainActivityTest {
         // Open the navigation drawer
         Espresso.onView(ViewMatchers.withId(R.id.drawer_layout))
                 .perform(DrawerActions.open());
+
+        // Click on the "Settings" item in the navigation drawer
+        Espresso.onView(ViewMatchers.withId(R.id.navigation_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_settings));
+
     }
 
 }
